b03770c979175a9cb92de869ba981205f583316c
Florence Loi <florence.c.loi@gmail.com>
2016-02-08 13:14:35 -0800

    Successfully made Yelp API calls in interactive mode

---
+++ server.py
@@ -0,0 +1,15 @@
+import io, json
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
+
+with io.open('yelp_secret.json') as cred:
+    print cred, "*****************"
+
+    # Loads in yelp_secret.json
+    creds = json.load(cred)
+
+    # Instantiates Oauth1Authenticator object with API keys
+    auth = Oauth1Authenticator(**creds)
+
+    # Constructs client object using Oauth1Authenticator object
+    client = Client(auth)
